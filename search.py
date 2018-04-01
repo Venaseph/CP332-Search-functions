004b5230275e4c0e143cd24b63030515caf42ce4
C.Peragine <cperagine@gmail.com>
2018-04-01 10:25:28 -0400

    final with comments and some formatting changes

--- search.py
+++ search.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import sys
 import argparse
 import re
@@ -9,20 +10,20 @@ def main():
     searchlist = createlist(args)
     search(args, searchlist)
 
-    return (0)
+    return 0
 
 
 # Handle Argobj setup/creation
 def argparser():
     parser = argparse.ArgumentParser(description='Search Function - Assignment 2')
-    parser.add_argument('--match', '-m', required='True', help='comma-seperated list of patterns to match (Required)')
+    parser.add_argument('--match', '-m', required='True', help='comma-separated list of patterns to match (Required)')
     parser.add_argument('--ignore-case', '-i', action='store_true', help='ignore upper/lower case')
     parser.add_argument('-r', action='store_true', help="""traverse all sub-directories recursively to search for 
     matches within any sub-directories""")
 
     args = parser.parse_args()
 
-    # Regex match Letters/Numbers/Commas
+    # Regex match Letters/Numbers/Commas/Periods
     if re.match('[a-zA-Z0-9,.]', args.match):
         return args
     else:
@@ -32,27 +33,34 @@ def argparser():
 
 # Argparse into List
 def createlist(args):
-    # Any internal - characters in an argparse will be converted to _
+    # Any internal - characters in an argparse name will be converted to _
     if args.ignore_case:
         searchstring = args.match.lower()
     else:
         searchstring = args.match
 
+    # split string on commas
     searchlist = searchstring.split(',')
     return searchlist
 
 
 # Dir/file search Handling
 def search(args, searchlist):
+    # get directory
     cwd = os.getcwd()
+
+    # lists for string storage
     foundfile = []
     founddir = []
 
     for term in searchlist:
+        # walk param handling for recursive implementation
         if args.r:
             searchparams = os.walk(cwd)
         else:
+            # next will only search starting dir
             searchparams = [next(os.walk(cwd))]
+
         for root, directory, file in searchparams:
             for fstring in file:
                 if term in fstring:
@@ -61,6 +69,7 @@ def search(args, searchlist):
                 if term in dstring:
                     founddir.append(os.path.join(root, dstring))
         printresults(foundfile, founddir, term)
+        # clear lists for next term
         foundfile.clear()
         founddir.clear()
 
